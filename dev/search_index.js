c084f585745f4419d588b60702b874f90f957c13
Documenter.jl <documenter@juliadocs.github.io>
2021-09-28 09:24:31 +0000

    build based on 5517bc3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WeaponSystems","category":"page"},{"location":"#WeaponSystems","page":"Home","title":"WeaponSystems","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WeaponSystems.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WeaponSystems]","category":"page"}]
+}
