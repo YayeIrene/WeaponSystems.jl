76b4c27fc1acab099781aed25a05e9188bf254ae
Irene <irene.ndindabahizi@gmail.com>
2021-10-06 11:35:53 +0200

    add tranformations

--- src/WeaponSystems.jl
+++ src/WeaponSystems.jl
@@ -3,6 +3,7 @@ module WeaponSystems
 # Write your package code here.
 include("types.jl")
 include("tank.jl")
+include("transformations.jl")
 
-export  TargetRect, TargetCirc, TargetSphe, Hull, Turret, Canon, Sight, Tank
+export  TargetRect, TargetCirc, TargetSphe, Hull, Turret, Canon, Sight, Tank, targetPos, muzzlePos, muzzleVel, wind
 end
